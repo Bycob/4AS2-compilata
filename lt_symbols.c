8b5a90a4990f6b1004ec1c0c60bbb4b915845266
Louis Jean <ljean@etud.insa-toulouse.fr>
2019-04-02 18:45:19 +0200

    symboala table gets a buff. It's now able to geto last itemaru, to pop temporary variable, and is strongly partially tested. ata.

--- lt_symbols.c
+++ lt_symbols.c
@@ -4,6 +4,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+int lt_sizeof(int type) {
+    switch (type) {
+        case TYPE_INT: return 4;
+        default: return -1;
+    }
+}
 
 void lt_init_table(lt_symbol_table *table) {
     table->parent_scope = NULL;
@@ -13,20 +19,39 @@ void lt_init_table(lt_symbol_table *table) {
     }
 }
 
-int lt_add_symbol(lt_symbol_table *table, int type, long addr, char* name) {
+void lt_free_table(lt_symbol_table *table) {
+    // result
+}
+
+int lt_add_symbol(lt_symbol_table *table, int type, char* name) {
     // find id for the new symbol
     int new_id = table->last_id;
 
-    if (new_id > SYMBOL_TABLE_SIZE) {
+    if (new_id >= SYMBOL_TABLE_SIZE) {
         return -1;
     }
 
+    // find address for the new symbol
+    int addr = lt_find_last_address(table);
+
     // add symbol
     table->array[new_id] = (lt_symbol) {
-        new_id, type, addr, strdup(name)
+        table->id_offset + new_id, type, addr, strdup(name)
     };
     table->last_id = new_id + 1;
     return 0;
+}
+
+lt_symbol *lt_get_symbol_by_id(lt_symbol_table *table, int id) {
+    if (id < 0) {
+        return NULL;
+    }
+    else if (id < table->id_offset) {
+        return lt_get_symbol_by_id(table->parent_scope, id);
+    }
+    else {
+        return &table->array[id - table->id_offset];
+    }
 }
 
 lt_symbol *lt_get_symbol_by_name(lt_symbol_table *table, char* name) {
@@ -46,6 +71,35 @@ lt_symbol *lt_get_symbol_by_name(lt_symbol_table *table, char* name) {
     return NULL;
 }
 
+lt_symbol *lt_get_last(lt_symbol_table *table) {
+    if (table->last_id == 0) {
+        if (table->parent_scope == NULL) {
+            return NULL;
+        }
+        else {
+            return lt_get_last(table->parent_scope);
+        }
+    }
+    else {
+        return &table->array[table->last_id - 1];
+    }
+}
+
+lt_symbol lt_pop(lt_symbol_table *table) {
+    if (table->last_id == 0) {
+        lt_symbol ret;
+        ret.id = -1;
+        return ret;
+    }
+    else {
+        lt_symbol *last = lt_get_last(table);
+        lt_symbol result = *last;
+        last->id = -1;
+        --table->last_id;
+        return result;
+    }
+}
+
 int lt_is_in_table(lt_symbol_table *table, char* name) {
     return lt_get_symbol_by_name(table, name) != NULL;
 }
@@ -54,6 +108,7 @@ void lt_open_scope(lt_symbol_table **table) {
     lt_symbol_table *new_scope = malloc(sizeof(lt_symbol_table));
     lt_init_table(new_scope);
     new_scope->parent_scope = (*table);
+    new_scope->id_offset = new_scope->parent_scope->id_offset + SYMBOL_TABLE_SIZE;
     *table = new_scope;
 }
 
@@ -65,4 +120,19 @@ void lt_close_scope(lt_symbol_table **table) {
     lt_symbol_table *parent_scope = (*table)->parent_scope;
     free(*table);
     *table = parent_scope;
+}
+
+int lt_find_last_address(lt_symbol_table *table) {
+    if (table->last_id == 0) {
+        if (table->parent_scope == NULL) {
+            return 0;
+        }
+        else {
+            return lt_find_last_address(table->parent_scope);
+        }
+    }
+    else {
+        lt_symbol *symbol = &table->array[table->last_id - 1];
+        return symbol->addr + lt_sizeof(symbol->type);
+    }
 }
--- lt_symbols.h
+++ lt_symbols.h
@@ -1,7 +1,12 @@
+#ifndef LT_SYMBOLS_H
+#define LT_SYMBOLS_H
 
 #define SYMBOL_TABLE_SIZE 512
 #define TYPE_VOID 0
 #define TYPE_INT 1
+
+int lt_sizeof(int type);
+
 
 struct lt_symbol_table;
 
@@ -13,6 +18,7 @@ typedef struct lt_symbol {
 } lt_symbol;
 
 typedef struct lt_symbol_table {
+	int id_offset;
 	int last_id;
     struct lt_symbol_table *parent_scope;
 	lt_symbol array[SYMBOL_TABLE_SIZE];
@@ -25,12 +31,25 @@ void lt_free_table(lt_symbol_table *table);
 
 /** Add a symbol in the table inside of the current scope. If the table
 is full, symbol is not added and this function returns -1. */
-int lt_add_symbol(lt_symbol_table *table, int type, long addr, char* name);
+int lt_add_symbol(lt_symbol_table *table, int type, char* name);
+
+lt_symbol *lt_get_symbol_by_id(lt_symbol_table *table, int id);
 
 lt_symbol *lt_get_symbol_by_name(lt_symbol_table *table, char* name);
+
+/** Get the last symbol of the table, ie the one with the highest id. */
+lt_symbol *lt_get_last(lt_symbol_table *table);
+
+/** Delete the last symbol of the table (with the highest id) and return it.  */
+lt_symbol lt_pop(lt_symbol_table *table);
 
 int lt_is_in_table(lt_symbol_table *table, char* name);
 
 void lt_open_scope(lt_symbol_table **table);
 
 void lt_close_scope(lt_symbol_table **table);
+
+/** Get address after the last symbol of the table. */
+int lt_find_last_address(lt_symbol_table *table);
+
+#endif // LT_SYMBOLS_H
